3782a1fda92a633571a06cf6a3c564a23c3ab2f8
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2025-11-08 18:20:07 +0700

    fix(wezterm): adjust font size for better readability

--- wezterm.lua
+++ wezterm.lua
@@ -2,32 +2,35 @@ local wezterm = require("wezterm")
 local constants = require("constants")
 local config = wezterm.config_builder()
 
+config.front_end = "WebGpu"
+-- config.use_ime = false
+
 -- Font settings
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 19
+config.font_size = 12
 -- config.line_height = 1.2
 
 -- Color scheme
 config.colors = {
-  foreground = "#CBE0F0",
-  background = "#011423",
-  cursor_bg = "#47FF9C",
-  cursor_border = "#47FF9C",
-  cursor_fg = "#011423",
-  selection_bg = "#033259",
-  selection_fg = "#CBE0F0",
-  ansi = { "#94A9C3", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-  brights = { "#94A9C3", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+	foreground = "#CBE0F0",
+	background = "#011423",
+	cursor_bg = "#47FF9C",
+	cursor_border = "#47FF9C",
+	cursor_fg = "#011423",
+	selection_bg = "#033259",
+	selection_fg = "#CBE0F0",
+	ansi = { "#94A9C3", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	brights = { "#94A9C3", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 
 -- Appearance settings
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 config.window_background_image = constants.bg_image
 config.window_background_opacity = 0.8
@@ -39,16 +42,33 @@ config.prefer_egl = true
 
 -- Key bindings
 config.keys = {
-  {
-    key = "LeftArrow",
-    mods = "CMD",
-    action = wezterm.action.SendString("\x1b[1;5D"),
-  },
-  {
-    key = "RightArrow",
-    mods = "CMD",
-    action = wezterm.action.SendString("\x1b[1;5C"),
-  },
+	{
+		key = "LeftArrow",
+		mods = "CMD",
+		action = wezterm.action.SendString("\x1b[1;5D"),
+	},
+	{
+		key = "RightArrow",
+		mods = "CMD",
+		action = wezterm.action.SendString("\x1b[1;5C"),
+	},
 }
+
+-- GUI startup event to maximize the window
+local mux = wezterm.mux
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
+local seen = {}
+wezterm.on("window-config-reloaded", function(window, pane)
+	local id = window:mux_window():window_id()
+	if not seen[id] then
+		seen[id] = true
+		wezterm.sleep_ms(80)
+		window:maximize()
+	end
+end)
 
 return config
